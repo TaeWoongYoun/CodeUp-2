3125e0a8da30f8c101393eb58cd38d1773e62c00
xodnd0507 <24457545yong@gmail.com>
2023-08-27 23:35:12 +0900

    초기 커밋.

---
+++ CU.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+//코드업 문제 1024 풀이(단어 1개 입력받아 나누어 출력하기) (첫 반복문,  for문 안에 내용 이해 필요)
+//int main() {
+//	char d[30], i;
+//	scanf("%s", &d);
+//	for (i=0; d[i] != '\0'; i++) {
+//		printf("\'%c\'\n", d[i]);
+//	}
+//	return 0;
+//}
+
+//코드업 문제 1025 풀이 (정수 1개 입력받아 나누어 출력하기) (코드 줄이기 연습)
+//int main() {
+//	int a, b, c, d, e;
+//	scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+//	printf("[%d]\n[%d]\n[%d]\n[%d]\n[%d]", a*10000,b*1000,c*100,d*10,e*1);
+//	return 0;
+//}
